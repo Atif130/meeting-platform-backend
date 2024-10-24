ebac5fd0f4be08bd6a880222be264823d87685d6
Atif130 <ec.atif.ziya@hotmail.com>
2024-10-24 15:35:48 +0530

    Update joinWhiteboard.js

--- public/joinWhiteboard.js
+++ public/joinWhiteboard.js
@@ -1,1 +1,17 @@
-
+var request = require("request");
+var options = {
+  "method": "POST",
+  "url": "https://api.netless.link/v5/rooms",
+  "headers": {
+  "token": "Your SDK Token",
+  "Content-Type": "application/json",
+  "region": "us-sv"
+  },
+  body: JSON.stringify({
+    "isRecord": false
+  })
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
